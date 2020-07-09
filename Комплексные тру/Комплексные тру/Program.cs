0da0118b16969da71b5a35ed6f6373ec9c0390d6
Timur Yagfarov <65065200+TheClockworkk@users.noreply.github.com>
2020-07-09 11:55:21 +0300

    Add files via upload

---
+++ Комплексные тру/Комплексные тру/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complexx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex complex = new Complex(6, 13);
+
+            Console.WriteLine(Complex.Arg(complex).GetDecimalRepresentation(8));
+
+            Console.ReadKey();
+        }
+    }
+}
